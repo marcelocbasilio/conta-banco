9599dbca82bccd40e37f1998eb8ea75ddd35cac4
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2024-06-16 19:06:00 -0300

    Demandas atendidas.

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        // TODO: Projeto criado
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("> Digite nome do cliente: ");
+        String nomeCliente = sc.nextLine();
+
+        System.out.print("> Digite o número da agência (xxx-x): ");
+        String agencia = sc.nextLine();
+
+        System.out.print("> Digite o número da conta: ");
+        Integer numeroConta = sc.nextInt();
+
+        System.out.print("> Digite o valor a depositar: ");
+        Double valorDepositar = sc.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", nomeCliente, agencia, numeroConta, valorDepositar);
+
+        sc.close();
     }
 
 }
